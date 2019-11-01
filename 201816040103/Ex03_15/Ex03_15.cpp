0422758a2b73dc295e35577717b719fc82c5cd10
201816040103 <56601097+201816040103@users.noreply.github.com>
2019-11-01 12:54:13 +0800

    Create Ex03_15.cpp

---
+++ 201816040103/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "Date.h"
+
+using namespace std;
+
+int main()
+{
+	Date Date1(11,4,2019);
+	Date Date2(15,4,2019);
+	
+	Date1.getdisplayDate();
+	Date2.getdisplayDate();
+}
