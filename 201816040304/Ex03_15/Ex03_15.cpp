018bace8ddbc46d5797cb96bfc2fb3f62e63fe2b
kore2048 <56683147+kore2048@users.noreply.github.com>
2019-11-01 19:27:15 +0800

    Create Ex03_15.cpp
    ex03_15.cpp
    student id 201816040304

---
+++ 201816040304/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,20 @@
+// Ex03_15: DateTest.cpp
+// Test program for modified Date class.
+#include <iostream>
+
+using namespace std;
+// include definition of class Date from Date.h
+#include "Date.h"
+
+// function main begins program execution
+int main()
+{
+    // create a Date object
+    Date date(2019,11,1);
+
+    // dispaly the message of the date
+    date.displaydate();
+
+
+    return 0;
+}// end main
